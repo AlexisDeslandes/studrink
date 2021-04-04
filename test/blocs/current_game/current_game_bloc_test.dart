567af904a6ddfc97b6c05fad51962c3099a5e659
alexisdeslandes <alexis.deslandes@planisware.com>
2021-04-04 18:01:31 +0200

    Add mocktail and fix test

--- test/blocs/current_game/current_game_bloc_test.dart
+++ test/blocs/current_game/current_game_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:ptit_godet/blocs/current_game/current_game_bloc.dart';
 import 'package:ptit_godet/blocs/dice/dice_bloc.dart';
 import 'package:ptit_godet/blocs/nav/nav_bloc.dart';
