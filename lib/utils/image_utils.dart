8e3956ccdaa6957de62b9d609aadc56e83967ca3
alexisdeslandes <alexis.deslandes@planisware.com>
2021-09-10 22:06:22 +0200

    Remove crop step in image taking, center automatically. Add a loading step on crop functioning.

---
+++ lib/utils/image_utils.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+import 'dart:typed_data';
+import 'package:image/image.dart' as Img;
+
+class ImageUtils {
+  ///
+  /// Crop the image represent by [bytes] removing
+  /// extra white space and transparency surrounded image.
+  ///
+  static Future<Uint8List> crop(Uint8List bytes) async {
+    final image = Img.bakeOrientation(Img.decodeImage(bytes)!);
+    final cropRectangle = _cropRectangle(image);
+    final cropImage = _cropImage(cropRectangle, image);
+    final newPng = Img.encodePng(cropImage);
+    return Uint8List.fromList(newPng);
+  }
+
+  static Rectangle<int> _cropRectangle(Img.Image src) {
+    final width = src.width, height = src.height, imgSize = width * 0.8;
+    int top = (height / 2 - imgSize / 2).toInt(),
+        bottom = (height / 2 + imgSize / 2).toInt(),
+        left = (width / 2 - imgSize / 2).toInt(),
+        right = (width / 2 + imgSize / 2).toInt();
+    return Rectangle(left, top, right - left, bottom - top);
+  }
+
+  static Img.Image _cropImage(Rectangle<int> cropRectangle, Img.Image image) {
+    final img = Img.Image(cropRectangle.width, cropRectangle.height),
+        top = cropRectangle.top,
+        left = cropRectangle.left;
+    for (var x = 0; x < cropRectangle.width; x++) {
+      for (var y = 0; y < cropRectangle.height; y++) {
+        img.setPixel(x, y, image.getPixel(x + left, y + top));
+      }
+    }
+    return img;
+  }
+}
--- lib/widgets/pre_game/fab_camera.dart
+++ lib/widgets/pre_game/fab_camera.dart
@@ -2,37 +2,64 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:studrink/blocs/current_game/current_game_bloc.dart';
 import 'package:studrink/models/player.dart';
+import 'package:studrink/utils/image_utils.dart';
 
-class FabCamera extends StatelessWidget {
+class FabCamera extends StatefulWidget {
   final Player player;
 
   const FabCamera({Key? key, required this.player}) : super(key: key);
 
   @override
+  _FabCameraState createState() => _FabCameraState();
+}
+
+class _FabCameraState extends State<FabCamera> {
+  late Player _player = widget.player;
+  bool _isLoading = false;
+
+  @override
+  void didUpdateWidget(covariant FabCamera oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.player.avatar != oldWidget.player.avatar) {
+      setState(() {
+        _player = widget.player;
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final avatarPlayer = player.avatar;
+    final avatarPlayer = _player.avatar;
     return Container(
       child: FloatingActionButton(
-          heroTag: "fab_camera_${player.id}",
+          backgroundColor:
+              _isLoading ? Theme.of(context).scaffoldBackgroundColor : null,
+          heroTag: "fab_camera_${_player.id}",
           onPressed: kIsWeb ? null : () => _takePicture(context),
           mini: true,
-          child: avatarPlayer != null
-              ? ClipRRect(
-                  child: Image.memory(avatarPlayer),
-                  borderRadius: BorderRadius.circular(100.0))
-              : Container(
-                  width: 48.0,
-                  height: 48.0,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100.0),
-                      color: player.color),
-                  child: kIsWeb
-                      ? null
-                      : Icon(Icons.camera_alt, color: Colors.black))),
+          child: _isLoading
+              ? Padding(
+                  padding: const EdgeInsets.all(3.0),
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).primaryColor),
+                )
+              : avatarPlayer != null
+                  ? ClipRRect(
+                      child: Image.memory(avatarPlayer),
+                      borderRadius: BorderRadius.circular(100.0))
+                  : Container(
+                      width: 48.0,
+                      height: 48.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100.0),
+                          color: _player.color),
+                      child: kIsWeb
+                          ? null
+                          : Icon(Icons.camera_alt, color: Colors.black))),
     );
   }
 
@@ -42,14 +69,11 @@ class FabCamera extends StatelessWidget {
             source: ImageSource.camera,
             preferredCameraDevice: CameraDevice.front);
     if (pickedFile != null) {
-      final croppedFile = await ImageCropper.cropImage(
-              sourcePath: pickedFile.path,
-              aspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
-              cropStyle: CropStyle.circle),
-          pictureArray = await croppedFile!.readAsBytes();
-      context
-          .read<CurrentGameBloc>()
-          .add(ChangePicturePlayer(player: player, pictureData: pictureArray));
+      setState(() => _isLoading = true);
+      final bytes = await pickedFile.readAsBytes();
+      final bytesCropped = await compute(ImageUtils.crop, bytes);
+      context.read<CurrentGameBloc>().add(ChangePicturePlayer(
+          player: widget.player, pictureData: bytesCropped));
     }
   }
 }
